3b8bc9953fd5d2d91971f811d99c4645db19ce3a
Hannes Lowette <lowette.hannes@gmail.com>
2024-06-10 12:46:50 +0200

    migrations working

--- src/Axxes.EfCore.Workshop.Domain/Models/Movie.cs
+++ src/Axxes.EfCore.Workshop.Domain/Models/Movie.cs
@@ -5,6 +5,7 @@ public class Movie
     public int Id { get; set; }
     public required string Title { get; set; }
     public DateOnly ReleaseDate { get; set; }
+    public string? Description { get; set; }
 
     public override string ToString()
     {
